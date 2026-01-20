8132ac6eef70e3335bbe77b05091189a59ab1db1
oussama tkitak <tkitakoussama8@gmail.com>
2026-01-20 12:29:06 +0100

    commit relations

--- projet2-laravel/app/Models/Commande.php
+++ projet2-laravel/app/Models/Commande.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Commande extends Model
 {
     use HasFactory;
+
+    public function Client(){
+        return $this->belongsToMany(Client::class) ;
+    }
 }
