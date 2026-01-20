22550803b9c86e2d3596e6a5c105d1b08422296c
oussama tkitak <tkitakoussama8@gmail.com>
2026-01-20 10:29:46 +0100

    test relations eloquent

---
+++ projet2-laravel/database/seeders/AdherentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdherentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
